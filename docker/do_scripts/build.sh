46af60550f1ef2e47781a92fe6451dda247dd4bd
Denis Iogansen <pofigizm@gmail.com>
2015-11-01 14:14:39 +0300

    fix node and npm versions in build container

--- docker/do_scripts/build.sh
+++ docker/do_scripts/build.sh
@@ -12,9 +12,15 @@ echo "-------------------- 2"
 
 echo "FROM ubuntu" > $DIR/Dockerfile
 echo "RUN apt-get update" >> $DIR/Dockerfile
-echo "RUN apt-get install -y nodejs nodejs-legacy npm git git-core python build-essential" >> $DIR/Dockerfile
+echo "RUN apt-get install -y curl wget git git-core" >> $DIR/Dockerfile
+echo "RUN apt-get install -y python build-essential" >> $DIR/Dockerfile
+echo "RUN curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -" >> $DIR/Dockerfile
+echo "RUN apt-get install -y nodejs" >> $DIR/Dockerfile
+echo "RUN npm install -g npm" >> $DIR/Dockerfile
+echo "RUN node -v" >> $DIR/Dockerfile
+echo "RUN npm -v" >> $DIR/Dockerfile
 echo "WORKDIR /src" >> $DIR/Dockerfile
-echo "CMD (git clone -b $BRANCH https://github.com/Pofigizm/666.git . || git pull) && npm install -verbose && npm run build" >> $DIR/Dockerfile
+echo "CMD (git clone -b $BRANCH https://github.com/Pofigizm/666.git . || git pull) && npm install && npm run build" >> $DIR/Dockerfile
 cat $DIR/Dockerfile
 
 echo "-------------------- 3"
